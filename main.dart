0abc6ad05edc4c934d62385fb2505a3ca6a95043
afgan galih <afgangalihf@gmail.com>
2025-09-17 15:49:28 +0700

    Add BMI calculation feature

--- main.dart
+++ main.dart
@@ -1,3 +1,29 @@
 void main() {
-  print("BMI Calculator App");
+  double tinggi = 170;
+  double berat = 65;
+
+  if (tinggi <= 0 || berat <= 0) {
+    print("Input tidak valid!");
+    return;
+  }
+
+  hitungBMI(tinggi, berat);
+}
+
+void hitungBMI(double tinggiCm, double beratKg) {
+  double tinggiM = tinggiCm / 100;
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
+  }
+
+  print("BMI: ${bmi.toStringAsFixed(2)}, Kategori: $kategori");
 }
